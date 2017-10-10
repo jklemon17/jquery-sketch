66e7363ed5108037e4e2a10f7f95c4a3cb0b11ef
Kyle Lemon <james.kyle.lemon@gmail.com>
2017-10-10 11:14:46 -0400

    Added color selectors (black or random).

--- javascript.js
+++ javascript.js
@@ -1,37 +1,66 @@
 $(function(){
   ResetGrid(16)
 
+  var colorSetting = 1
+
   $("#resetbtn").click(function(){
     ResetGrid(GetBoxesPerSide());
   });
 
+  $("#blackbtn").click(function(){
+    SetColorSetting(1);
+  });
+
+  $("#randombtn").click(function(){
+    SetColorSetting(2);
+  });
+
+  function SetColorSetting(n){
+    colorSetting=n
+  }
+
   function GetBoxesPerSide(){
-    var n=prompt("How many divisions do you want per side?");
+    var n=prompt("How many divisions do you want per side? (Default=16)",16);
     return n;
   }
 
-  function ResetGrid(n){
-    $("#cont").empty();
-    /*$(".boxes").css("background-color", "white");*/
-    /*var n=prompt("How many divisions do you want per side?");*/
-    var bsize = 30*16/n;
-    i=0;
-    while(i < n*n){
-      if(i%n===0){
-        $("#cont").append("<div class='boxes' style='clear:left;'></div>");
-      }
-      else{
-        $("#cont").append("<div class='boxes'></div>");
-      }
-      i++;
+  function SetColor(){
+    if (colorSetting==1){
+      $(event.target).css("background-color","black");
+    }
+    else{
+      var hue = parseInt(Math.random()*360);
+      var sat = parseInt(Math.random()*100);
+      var light = parseInt(Math.random()*100);
+
+      $(event.target).css("background-color","hsl("+hue+", "+sat+"%, "+light+"%)");
     };
+  }
 
-    $(".boxes").width(bsize);
-    $(".boxes").height(bsize);
+  function ResetGrid(n){
+    if(n==null){
+    }
+    else{
+      $("#cont").empty();
+      var bsize = 30*16/n;
+      i=0;
+      while(i < n*n){
+        if(i%n===0){
+          $("#cont").append("<div class='boxes' style='clear:left;'></div>");
+        }
+        else{
+          $("#cont").append("<div class='boxes'></div>");
+        }
+        i++;
+      };
 
-    $(".boxes").mouseenter(function(){
-      $(this).css("background-color","black");
-    });
+      $(".boxes").width(bsize);
+      $(".boxes").height(bsize);
+
+      $(".boxes").mouseenter(function(){
+        SetColor();
+      });
+    };
   };
 
 });
